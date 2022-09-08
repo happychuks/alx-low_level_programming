69931f78104a004992e6583037d76b1e46b0c241
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-08 15:44:45 -0700

    Write a C program that prints out a string without using printf or puts function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 /**
  * main - Entry point
  *
- * write: this takes three arguments,arg1 is the file descriptor for stdout, arg2 is the input string, arg3 returns the number of bytes written, or in case of error, a negative value. 
  * Return: returns 1 as Success
  */
 int main(void)
 {
-	char texts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, texts, 59);
+	write(2, str, 59);
 	return (1);
 }
