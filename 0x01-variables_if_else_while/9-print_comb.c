258f81b850c47268769fae449ab05a5f2aab5e5b
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-09 20:58:03 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
