98dda7e4da3f6e8bc78a5c87f28cdd87b89a12c6
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-09 20:41:23 -0700

    Base 16 numbers and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char base_16;
+
+	/*for base 16 0-9*/
+	for (base_16 = 48; base_16 <= 57; base_16++)
+	{
+		putchar(base_16); /*print 0-9*/
+	}
+
+	/*for base 16 a-f*/
+	for (base_16 = 97 ; base_16 <= 102; base_16++)
+	{
+		putchar(base_16); /*print a-f*/
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
