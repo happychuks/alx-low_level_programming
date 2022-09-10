16a6e76b4f0992e58e18f7ade30978ed8b23caaa
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-09 20:31:17 -0700

    Reverse letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char letter; 
+
+	/*for z-a*/
+	for (letter = 122 ; letter >= 97; letter--) /* using ASCII representation of letters*/
+	{
+		putchar(letter);
+	}
+
+	putchar('\n'); 
+
+	return (0);
+
+}
