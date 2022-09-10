09afb11ced6863f6cf7fe6391dc04706428dd782
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-09 20:20:28 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point This function prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
