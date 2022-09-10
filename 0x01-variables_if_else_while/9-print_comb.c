31b274aa37e3cfb4c4837130f6a523debcd65b7a
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-09 21:06:37 -0700

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,13 @@
 int main(void)
 {
 	int num;
-	num = 0;
-
-	while (num < 10)
+	
+	for (num = 0; num < 10; num++)
 	{
 		putchar(num);
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
 
 	return (0);
