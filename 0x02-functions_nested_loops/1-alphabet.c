d20a4b4a81cbadd5620cac552e23239c360190e4
Happy Felix Chukwuma <happychukwuma@gmail>
2022-09-13 15:34:15 -0700

    print alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> This function prints alphabets in lower case
+ *
+ * Return: 0 if Success
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
