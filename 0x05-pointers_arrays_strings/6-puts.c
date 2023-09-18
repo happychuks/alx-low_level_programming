dd8fa505c8b68a989c8cadfb2e6e00fe95fead36
Happy Chukwuma <happychukwuma@gmail.com>
2023-09-18 07:55:03 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print one of two char of a string
+ * @str: char array string type
+ * Description: Print first char then third, then fifth...
+ */
+
+void puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
