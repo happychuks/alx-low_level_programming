f63d81041f2d5992003a48a9de321d141ad7c6c4
Happy Chukwuma <happychukwuma@gmail.com>
2023-10-03 08:25:41 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 /**
  * *create_array - creates an array of chars,
  * and initializes it with a specific char
- * @size: size of the array to create
+ * @size: size of the array to be created
  * @c: char to initialize the array c
  *
  * Return: pointer to the array (Success), NULL (Error)
