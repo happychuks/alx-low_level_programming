29b9bdd0d131edaf31e89cf8f4fc701d620e9529
Happy Chukwuma <happychukwuma@gmail.com>
2023-10-19 18:09:04 -0700

    Task 1 and updated Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
- * Returns: Number of nodes printed
+ * Return: Number of nodes printed
  */
 
 size_t print_list(const list_t *h)
