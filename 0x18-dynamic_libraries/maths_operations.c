6da3f7d9b9ddca1dfdaa886a026dec6e16932e96
Happy Felix Chukwuma <happychukwuma@gmail>
2023-12-12 04:39:10 -0800

    Task 100

---
+++ 0x18-dynamic_libraries/maths_operations.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * @add(): function that add 2 int together
+ * @sub(): function that subtract 1 int from other
+ * @mul(): function that multiples 2 int
+ * @div(): function that divides 2 int
+ * @mod(): function that finds the modulus of 2 int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
